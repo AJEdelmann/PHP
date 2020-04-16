1a0d1798a0883466155650c6e83ea031b6cb7094
Albert Edelmann <albert.edelmann@gmail.com>
2020-04-16 20:12:42 +0200

    added Rerouting

---
+++ rerouting.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rerouting</title>
+</head>
+<body>
+    <?php
+    $validation_error = "";
+    $username = "";
+    $users = ["coolBro123" => "password123!", "coderKid" => "pa55w0rd*", "dogWalker" => "ais1eofdog$"];
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $username = $_POST["username"];
+        $password  = $_POST["password"];
+        if (isset($users[$username]) && $users[$username] === $password){
+        header("Location: success.html");
+            exit;        
+        } else {
+        $validation_error = "* Incorrect username or password.";
+        }
+    }
+    ?>
+    
+    <h3>Welcome back!</h3>
+    <form method="post" action="">
+        Username:<input type="text" name="username" value="<?php echo $username;?>">
+        <br>
+        Password:<input type="text" name="password" value="">
+        <br>
+        <span class="error"><?= $validation_error;?></span>
+        <br>
+        <input type="submit" value="Log in">
+    </form>
+</body>
+</html>
