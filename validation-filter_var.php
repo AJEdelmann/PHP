a28778e30301acee309f8fe7023eac07a9eaef7f
Albert Edelmann <albert.edelmann@gmail.com>
2020-04-16 19:56:29 +0200

    added validation-filter_var

---
+++ validation-filter_var.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validation Filter_Var</title>
+</head>
+<body>
+    <?php
+    $validation_error = "";
+    $user_url = "";
+    $form_message = "";
+
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $user_url = $_POST["url"];
+    if(!filter_var($user_url, FILTER_VALIDATE_URL)) {
+        $validation_error = "* This is an invalid URL.";
+        $form_message = "Please retry and submit your form again.";
+    } else {
+        $form_message = "Thank you for your submission.";
+    }
+    }
+
+    ?>
+
+    <form method="post" action="">
+        Your Favorite Website: 
+        <br>
+        <input type="text" name="url" value="<?php echo $user_url;?>">
+        <span class="error"><?= $validation_error;?></span>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+    <p> <?= $form_message;?> </p> 
+    
+</body>
+</html>
